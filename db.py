8051fbbfab8dc310f8dea2f3c6ce9b13318eaf0a
Grambo <grambo@grambo.me.uk>
2012-03-05 22:50:28 +0000

    Added basic sqlalchemy ORM database structure

---
+++ db.py
@@ -0,0 +1,87 @@
+'''
+This file contains the database code
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+all = [
+    'GetDbSession',
+    'Compiler',
+    'CompilerVersion',
+    'Feature',
+    'FeatureCompilerVersion'
+    ]
+
+engine = create_engine( 'sqlite:///data.db', echo=True )
+GetDbSession = sessionmaker( bind=engine )
+
+Base = declarative_base()
+
+class Compiler(Base):
+    '''
+    Class representing a compiler
+    '''
+
+    __tablename__ = "Compiler"
+
+    id = Column( Integer, primary_key=True )
+    name = Column( String )
+
+class CompilerVersion(Base):
+    '''
+    Class representing an individual version of a compiler
+    '''
+
+    __tablename__ = "CompilerVersion"
+
+    id = Column( Integer, primary_key=True )
+    num = Column( String )
+    compilerId = Column(
+            Integer,
+            ForeignKey( 
+                'Compiler.id', onupdate='CASCADE', ondelete='CASCADE'
+                )
+            )
+    compiler = relationship(
+            'Compiler',
+            backref=backref( 'versions', passive_deletes=True )
+            )
+
+class Feature(Base):
+    '''
+    Class representing an individual compiler feature
+    '''
+
+    __tablename__ = "Feature"
+
+    id = Column( Integer, primary_key=True )
+    name = Column( String )
+    proposal = Column( String )
+    link = Column( String )
+
+class FeatureCompilerVersion(Base):
+    '''
+    Class linking features to their minimum compiler version
+    '''
+
+    __tablename__ = "FeatureCompilerVersion"
+
+    id = Column( Integer, primary_key=True )
+    featureId = Column(
+            Integer, 
+            ForeignKey( 
+                'Feature.id', ondelete='CASCADE', onupdate='CASCADE'
+                )
+            )
+    minimumVersionId = Column(
+            Integer,
+            ForeignKey(
+                "CompilerVersion.id", ondelete="CASCADE", onupdate="CASCADE"
+                )
+            )
+
+    feature = relationship( "Feature", backref="support" )
+    minimumVersion = relationship( "CompilerVersion" )
